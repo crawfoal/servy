cda67ac2b3133497f7fc9840e7a29130b6a32916
Amanda Dolan <amandacrawfordalc@gmail.com>
2018-10-08 12:06:36 -0400

    Ex 21 - make an API call; refactor

--- lib/servy/image_api.ex
+++ lib/servy/image_api.ex
@@ -2,13 +2,18 @@ defmodule Servy.ImageApi do
   @base_url "https://api.myjson.com/bins/"
 
   def query(image_name) do
-    case HTTPoison.get "#{@base_url}#{image_name}" do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        {:ok, Poison.Parser.parse!(body) |> get_in(["image", "image_url"])}
-      {:ok, %HTTPoison.Response{status_code: status, body: body}} ->
-        {:error, Poison.Parser.parse!(body) |> Map.get("message")}
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        {:error, reason}
-    end
+    HTTPoison.get("#{@base_url}#{image_name}") |> extract_path
+  end
+
+  defp extract_path({:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
+    {:ok, Poison.Parser.parse!(body) |> get_in(["image", "image_url"])}
+  end
+
+  defp extract_path({:ok, %HTTPoison.Response{status_code: _, body: body}}) do
+    {:error, Poison.Parser.parse!(body) |> Map.get("message")}
+  end
+
+  defp extract_path({:error, %HTTPoison.Error{reason: reason}}) do
+    {:error, reason}
   end
 end
