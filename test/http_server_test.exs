4911d3396a0cd9dc5dfb67352a6261edcb637eef
Amanda Dolan <amandacrawfordalc@gmail.com>
2018-10-10 09:47:44 -0400

    Lesson 22 - demo message sending and receiving

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -12,4 +12,27 @@ defmodule HttpServerTest do
     assert response.status_code == 200
     assert response.body == "Bears, Lions, Tigers"
   end
+
+  test "demonstrate message sending" do
+    spawn(HttpServer, :start, [5000])
+    parent = self()
+    max_concurrent_requests = 5
+
+    # Spawn processes that each send a request to HttpServer
+    for _ <- 1..max_concurrent_requests do
+      spawn(fn ->
+        {:ok, response} = HTTPoison.get('http://localhost:5000/wildthings')
+        send(parent, {:ok, response})
+      end)
+    end
+
+    # Wait for each message from the spawned processes, and check that the
+    # message includes a successful response.
+    for _ <- 1..max_concurrent_requests do
+      receive do
+        {:ok, response} ->
+          assert response.status_code == 200
+      end
+    end
+  end
 end
