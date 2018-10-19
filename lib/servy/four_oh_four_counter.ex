95f4225ebeb03fa1febf0dcedff698e7dcfe219b
Amanda Dolan <amandacrawfordalc@gmail.com>
2018-10-19 08:44:25 -0400

    Add a process to keep track of requests to non-existent routes

---
+++ lib/servy/four_oh_four_counter.ex
@@ -0,0 +1,44 @@
+defmodule Servy.FourOhFourCounter do
+  @name __MODULE__
+
+  # Client interface functions
+  def start do
+    pid = spawn(__MODULE__, :listen_loop, [%{}])
+    Process.register(pid, @name)
+    pid
+  end
+
+  def bump_count(path) do
+    send @name, {self(), :bump_count, path}
+
+    receive do {:response} -> :ok end
+  end
+
+  def get_count(path) do
+    send @name, {self(), :get_count, path}
+
+    receive do {:response, count} -> count end
+  end
+
+  def get_counts do
+    send @name, {self(), :get_counts}
+
+    receive do {:response, count_map} -> count_map end
+  end
+
+  # Server interface fucntions
+  def listen_loop(state) do
+    receive do
+      {sender, :bump_count, path} ->
+        state = state |> Map.update(path, 1, &(&1 + 1))
+        send sender, {:response}
+        listen_loop(state)
+      {sender, :get_count, path} ->
+        send sender, {:response, state[path]}
+        listen_loop(state)
+      {sender, :get_counts} ->
+        send sender, {:response, state}
+        listen_loop(state)
+    end
+  end
+end
