7e2c8480f76eda47649d1601c7af24c2a720b848
Amanda Dolan <amandacrawfordalc@gmail.com>
2018-10-26 11:21:53 -0400

    Lesson 25 - Whoops, call and cast should go in GenericServer as well

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -27,39 +27,7 @@ defmodule Servy.GenericServer do
         listen_loop(state, callback_module)
     end
   end
-end
 
-defmodule Servy.PledgeServer do
-  @name __MODULE__
-
-  alias Servy.GenericServer
-
-  # Client interface functions
-  def start do
-    GenericServer.start(__MODULE__, @name, [])
-  end
-
-  def stop do
-    GenericServer.stop(@name)
-  end
-
-  def create_pledge(name, amount) do
-    call(@name, {:create_pledge, name, amount})
-  end
-
-  def recent_pledges do
-    call(@name, :recent_pledges)
-  end
-
-  def total_pledged do
-    call(@name, :total_pledged)
-  end
-
-  def clear do
-    cast(@name, :clear)
-  end
-
-  # Helper Functions
   def call(name, message) do
     send name, {self(), :call, message}
 
@@ -71,6 +39,40 @@ defmodule Servy.PledgeServer do
   def cast(name, message) do
     send name, {:cast, message}
   end
+end
+
+defmodule Servy.PledgeServer do
+  @name __MODULE__
+
+  alias Servy.GenericServer
+
+  # Client interface functions
+
+  def start do
+    GenericServer.start(__MODULE__, @name, [])
+  end
+
+  def stop do
+    GenericServer.stop(@name)
+  end
+
+  def create_pledge(name, amount) do
+    GenericServer.call(@name, {:create_pledge, name, amount})
+  end
+
+  def recent_pledges do
+    GenericServer.call(@name, :recent_pledges)
+  end
+
+  def total_pledged do
+    GenericServer.call(@name, :total_pledged)
+  end
+
+  def clear do
+    GenericServer.cast(@name, :clear)
+  end
+
+  # Callback Functions
 
   def handle_call({:create_pledge, name, amount}, state) do
     {status, _} = post_pledge_to_service(name, amount)
